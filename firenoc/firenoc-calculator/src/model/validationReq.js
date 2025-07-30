3eb39deb010d3e0a34662a7961bde846077ee920
JithendarKumar-eGov <jithendar.k@egovernments.org>
2025-07-30 15:13:27 +0530

    #CCSD-370 updated building name acceptance value

--- firenoc/firenoc-calculator/src/model/validationReq.js
+++ firenoc/firenoc-calculator/src/model/validationReq.js
@@ -186,10 +186,10 @@ const Address = {
       description: "more address detail as may be needed"
     },
     buildingName: {
-      type: "string",
+      type: ["string", "null"],
       description: "Name of the building",
       maxLength: 64,
-      minLength: 2
+      minLength: 0
     },
     street: {
       type: "string",
