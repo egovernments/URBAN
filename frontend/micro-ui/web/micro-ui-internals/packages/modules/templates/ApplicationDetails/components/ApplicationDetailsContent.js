36916ae015f4ed812e704b7c420bf4cb1bbbbd63
EY-DIGIT <manish.1@in.ey.com>
2025-08-11 17:22:41 +0530

    reveting changes shubham

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/components/ApplicationDetailsContent.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/templates/ApplicationDetails/components/ApplicationDetailsContent.js
@@ -701,7 +701,7 @@ const ApplicationDetailsContent = ({
   oldValue,
   isInfoLabel = false
 }) => {
-  const [printing, setPrinting] = useState(false);
+    const [printing, setPrinting] = useState(false);
   const [manualAmount, setManualAmount] = useState("");
   const [showAssessmentPop, setShowAssesmentPop] = useState(false);
   const [selectedModes, setSelectedModes] = useState([]);
@@ -1013,9 +1013,8 @@ const ApplicationDetailsContent = ({
   const tenantIdEs = state?.tenantId || __tenantId || Digit.ULBService.getCurrentTenantId();
   const propertyId = state?.propertyId;
   const stateTenant = Digit.ULBService.getStateId();
-
+  // const { control, handleSubmit } = useForm();
   const { data: menu, isLoading } = Digit.Hooks.useCommonMDMS(stateTenant, "DIGIT-UI", "PaymentGateway");
-
   const { data: paymentdetails, isLoading: paymentLoading } = Digit.Hooks.useFetchPayment(
     { tenantId: tenantIdEs, consumerCode: wrkflow === "WNS" ? connectionNo : consumerCode, businessService },
     {}
@@ -1113,19 +1112,18 @@ const ApplicationDetailsContent = ({
     select: (data) =>
       data["common-masters"]?.uiCommonPay?.filter(({ code }) => businessService?.includes(code))[0]?.receiptKey || "consolidatedreceipt",
   });
-  const printReciept = async () => {
+    const printReciept = async () => {
     const tenantId = Digit.ULBService.getCurrentTenantId();
     const state = Digit.ULBService.getStateId();
     const payments = await Digit.PaymentService.getReciept(tenantId, businessService, { receiptNumbers: receiptNumber });
     let response = { filestoreIds: [payments.Payments[0]?.fileStoreId] };
 
     if (!payments.Payments[0]?.fileStoreId) {
-      response = await Digit.PaymentService.generatePdf(state,
-        {
-          Payments: payments.Payments,
-          Calculation: estimateData?.Calculation?.[0]
-        }
-        , generatePdfKey);
+        const paymentsWithCalculation = payments.Payments.map(payment => ({
+      ...payment,
+      Calculation: estimateData?.Calculation?.[0] || {},
+    }));
+      response = await Digit.PaymentService.generatePdf(state, { Payments: paymentsWithCalculation }, generatePdfKey);
     }
     const fileStore = await Digit.PaymentService.printReciept(state, { fileStoreIds: response.filestoreIds[0] });
     window.open(fileStore[response.filestoreIds[0]], "_blank");
