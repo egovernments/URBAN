e9313a8f8fa034599c5b51fd7fc79e4c9f79098c
Hari-egov <hariprasad@egovernments.org>
2025-09-03 17:00:56 +0530

    CSS chages

--- frontend/mono-ui/web/rainmaker/dev-packages/egov-firenoc-dev/src/ui-molecules-local/HowItWorks/index.js
+++ frontend/mono-ui/web/rainmaker/dev-packages/egov-firenoc-dev/src/ui-molecules-local/HowItWorks/index.js
@@ -24,6 +24,20 @@ const styles = theme => ({
   },
   stepContent: {
     paddingLeft: "24px"
+  },
+  jss16: {
+    margin: "1rem 1rem",
+    display: "block"
+  },
+  jss171: {
+    fill: "currentColor",
+    width: "1em",
+    height: "1em",
+    display: "inline-block",
+    fontSize: "28px",
+    transition: "fill 200ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+    userSelect: "none",
+    flexShrink: 0
   }
 });
 
