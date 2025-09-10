ee292cadfb0b5d22ac0be268172beee8a2917704
Hari-egov <hariprasad@egovernments.org>
2025-09-10 12:50:11 +0530

    login-fix

--- frontend/micro-ui/web/micro-ui-internals/example/public/build-info.js
+++ frontend/micro-ui/web/micro-ui-internals/example/public/build-info.js
@@ -1,7 +1,7 @@
 // Auto-generated build information
 window.DIGIT_UI_BUILD_INFO = {
   "version": "1.5.4",
-  "buildTime": "2025-09-10T00:00:00.000Z",
-  "buildId": "dev-build",
+  "buildTime": "2025-09-10T06:08:39.171Z",
+  "buildId": "0535deb4",
   "gitCommit": "unknown"
 };
